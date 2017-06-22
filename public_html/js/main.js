0af0e960f80047e7bce5033c21afb42646d854ac
bojant987 <bojant987@hotmail.com>
2017-06-22 19:06:48 +0200

    product-list page done

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   $("body").on("click", ".popin-handler", function(e) {
     var role = $(this).attr("data-role");
     var target = $(this).attr("data-target");
-
+    e.preventDefault();
     if (role === "open") {
       $(".navigation-left").removeClass("navigation-show");
       $(".login-box").removeClass("login-box-show");
@@ -32,7 +32,7 @@ $(document).ready(function() {
   // window resize changes
   $(window).resize(function() {
 
-    if ($(window).width() > 992) {
+    if ($(window).width() > 991) {
       $(".navigation-left").removeClass("navigation-show");
       $(".overlay").hide();
     }
@@ -53,6 +53,7 @@ $(document).ready(function() {
     navDropdownHover();
   });
 
+  // on scroll position updates
   $(document).scroll(function() {
 
     setTimeout(function functionName() {
@@ -90,28 +91,30 @@ $(document).ready(function() {
     $(".search-box").fadeToggle();
   });
 
-  // accordion
+  // nav accordion
   $(".accordion-handler").click(function(e) {
     var accordionLevel = $(this).next().attr("data-accordion");
     var target = $(this).attr("data-target");
     var currentTarget = $(this);
+    var location = $(this).next().attr("data-location");
+    console.log(location);
 
-    $(this).toggleClass("show");
+    $(this).toggleClass("show-mode");
 
     if (target === "level3") {
 
-      $(this).parent().siblings("li").find(".accordion-handler").removeClass("show");
+      $(this).parent().siblings("li").find(".accordion-handler").removeClass("show-mode");
 
-      $(".accordion-content").each(function() {
+      $(location + " .accordion-content").each(function() {
         if ($(this).attr("data-accordion") === "level3") {
           $(this).slideUp();
         }
       });
 
     } else if (target === "level2") {
-      $(this).parent().siblings("li").find(".accordion-handler").removeClass("show");
+      $(this).parent().siblings("li").find(".accordion-handler").removeClass("show-mode");
 
-      $(".accordion-content").each(function() {
+      $(location + " .accordion-content").each(function() {
 
         if ($(this).attr("data-accordion") === "level2") {
           $(this).slideUp();
@@ -119,33 +122,41 @@ $(document).ready(function() {
       });
 
     } else {
-      $(".accordion-content").slideUp();
-      $(".accordion-handler").each(function() {
+
+      $(location + " .accordion-content").each(function() {
+
+        if ($(this).prev().attr("data-anim") === "no-animation") {
+          // (BUG) hide wont work, like the element does not exist in the DOM at the time or something but it does
+          $(this).fadeOut(100);
+        } else {
+          $(this).slideUp();
+        }
+
+      });
+
+      $(location + " .accordion-handler").each(function() {
         if ($(this).get(0) !== currentTarget.get(0)) {
-          $(this).removeClass("show");
+          $(this).removeClass("show-mode");
         }
       });
 
     }
 
     if ($(this).next().is(":hidden")) {
-      $(this).next().slideDown();
+
+      if ($(this).attr("data-anim") === "no-animation") {
+        $(this).next().show();
+      } else {
+        $(this).next().slideDown();
+      }
+
     }
 
   });
 
-  // nav dropdown
+  // nav dropdown on hover
   function navDropdownHover() {
-    // $(".navigation-left > ul > li").mouseenter(function() {
-    //   if ($(window).width() > 991) {
-    //     $(this).find(".nav-dropdown").fadeIn(300);
-    //
-    //     $(this).mouseleave(function() {
-    //       $(this).find(".nav-dropdown").hide();
-    //     });
-    //   }
-    //
-    // });
+
     if ($(window).width() > 991) {
 
       var hover = $(".navigation-left > ul > li").mouseenter(function() {
@@ -154,7 +165,7 @@ $(document).ready(function() {
         $(".has-arrow").removeClass("has-arrow");
         $(".has-arrow").removeClass("has-arrow-colored");
 
-        $(this).find(".nav-dropdown").fadeIn(300);
+        $(this).find(".nav-dropdown").show();
 
         if ($(this).find(".nav-dropdown").hasClass("three-leveled")) {
           $(this).find(">a").addClass("has-arrow-colored");
@@ -178,6 +189,7 @@ $(document).ready(function() {
   };
   navDropdownHover();
 
+  // change position to static on large screen so we can position dropdown properly
   if ($(window).width() > 991) {
     $(".navigation-left > ul > li").css("position", "static");
   } else {
@@ -211,16 +223,6 @@ $(document).ready(function() {
       $("header .upper-header").show();
     }
   });
-
-  // nav arrow position
-
-  // function navArrowPosition() {
-  //   $(".navigation-left > li").each(function() {
-  //     var itemPosition = $(this).position();
-  //     $(this).find(".navigation-left");
-  //   });
-  // }
-  // $();
 
   // empty cart-box rendering
   function emptyCart() {
@@ -309,6 +311,420 @@ $(document).ready(function() {
   // this event does not want to work for some reason
   $(".newsletter-form input[type=email]").on("valid.bs.validator", function() {
     $(this).removeClass("email-invalid");
+  });
+
+
+  // swap img on product hover
+  $(".product .upper").mouseenter(function() {
+    var orgSrc = $(this).find("img").attr("src");
+    var hoverSrc = orgSrc.replace(".jpg", "-hov.jpg");
+
+    $(this).find("img").attr("src", hoverSrc);
+
+    $(this).mouseleave(function functionName() {
+      $(this).find("img").attr("src", orgSrc);
+    });
+
+  });
+
+  // addto-wishlist hover
+  $(".product .wishlist-handler").mouseenter(function() {
+
+    if ($(this).attr("data-role") === "add") {
+
+      $(this).find(".icon-wishlist_outline").css("display", "none");
+      $(this).find(".icon-wishlist").css("display", "block");
+
+      $(this).find(".add").show();
+
+    } else if ($(this).attr("data-role") === "remove") {
+
+      $(this).find(".remove").show();
+
+    }
+
+  });
+
+  $(".product .wishlist-handler").mouseleave(function() {
+
+    if ($(this).attr("data-role") === "add") {
+
+      $(this).find(".icon-wishlist_outline").css("display", "block");
+      $(this).find(".icon-wishlist").css("display", "none");
+
+      $(this).find(".pop-over").hide();
+
+    } else if ($(this).attr("data-role") === "remove") {
+
+      $(this).find(".pop-over").hide();
+
+    }
+
+  });
+
+  // add/remove from wishlist
+  $(".wishlist-handler").click(function(e) {
+    e.preventDefault();
+    var item = $(this).parent().parent().siblings(".lower").find(".name").text();
+
+    if ($(this).attr("data-role") === "add") {
+
+      $(this).find(".icon-wishlist_outline").css("display", "none");
+      $(this).find(".icon-wishlist").css("display", "block");
+      $(this).find(".add").hide();
+      $(this).find(".remove").show();
+      $(this).attr("data-role", "remove");
+
+      $(".added-removed").html(item + " has been added to your wishlist");
+      showAddedRemoved();
+
+    } else if ($(this).attr("data-role") === "remove") {
+
+      $(this).find(".remove").hide();
+      $(this).find(".add").show();
+      $(this).attr("data-role", "add");
+
+      $(".added-removed").html(item + " has been removed from your wishlist");
+      showAddedRemoved();
+
+    }
+
+  });
+
+  // show added-removed
+  function showAddedRemoved() {
+    $(".added-removed").fadeIn();
+
+    setTimeout(function functionName() {
+      $(".added-removed").fadeOut();
+    }, 2000);
+  }
+
+  // cross product original price if discount exists
+  $(".product").each(function() {
+    if ($(this).find(".price-discount").html() !== "" && $(this).find(".price-discount").html() !== undefined) {
+      $(this).find(".price-original").css("text-decoration", "line-through");
+    }
+  });
+
+  // back to top button
+  $(document).scroll(function() {
+
+    if ($(document).scrollTop() > 1200) {
+      $(".back-to-top").fadeIn();
+    } else {
+      $(".back-to-top").fadeOut();
+    }
+
+  });
+
+  $(".back-to-top").click(function(e) {
+    e.preventDefault();
+
+    $("html, body").animate({
+      scrollTop: 0
+    }, 800);
+
+  });
+
+  // filter fake checkboxes
+  $(".accordion-content-filters").on("change", "input", function() {
+
+    if ($(this).is(":checked")) {
+      $(this).next().addClass("checked");
+    } else {
+      $(this).next().removeClass("checked");
+    }
+
+    renderButton();
+
+  });
+
+  // render apply or close button for filters
+  function renderButton() {
+    var counter = 0;
+    $(".accordion-content-filters input[type=checkbox]").each(function() {
+      if ($(this).is(":checked")) {
+        counter++;
+      }
+    });
+
+    if ($(".accordion-content-filters").attr("data-opened") === "sort" && $(window).width() < 992) {
+      $(".accordion-content-filters .apply-btn").hide();
+      $(".accordion-content-filters .close-btn").hide();
+    } else {
+
+      if (counter === 0) {
+        $(".accordion-content-filters .apply-btn").hide();
+        $(".accordion-content-filters .close-btn").show();
+      } else {
+        $(".accordion-content-filters .close-btn").hide();
+        $(".accordion-content-filters .apply-btn").show();
+      }
+
+    }
+
+  };
+
+  // show all filters on small screens
+  $(".filters .show-filters").click(function() {
+    var target = $(this).attr("data-target");
+
+    $(".accordion-filter-sort").hide();
+    $(".sort .accordion-handler-filter").removeClass("show-mode");
+
+    if ($(".accordion-content-filters").attr("data-opened") === target || $(".accordion-content-filters").attr("data-opened") === "") {
+      $(".accordion-content-filters").toggle();
+    }
+
+    $(".accordion-content-filters .accordion-handler-filter").toggle();
+    $(this).toggleClass("show-mode");
+
+    if ($(".accordion-content-filters").is(":hidden")) {
+      $(".accordion-content-filters").attr("data-opened", "");
+    } else {
+      $(".accordion-content-filters").attr("data-opened", target);
+    }
+
+    renderButton();
+
+    // $(".products-filter-top-level > .container > .row > .col-xs-6:nth-of-type(2)").toggleClass("add-border");
+
+
+  });
+
+  // filter dropdown on small screens(<992px)
+  $(".accordion-content-filters .accordion-handler-filter").click(function() {
+    var target = $(this).attr("data-target");
+    var clicked = $(this);
+
+    $(".accordion-content-filters .accordion-handler-filter").each(function() {
+      if (!$(this).is(clicked)) {
+        $(this).removeClass("show-mode");
+        $(this).next().slideUp();
+      }
+    });
+    $(this).next().slideToggle();
+    $(this).toggleClass("show-mode");
+
+
+  });
+
+  // filter dropdown on large screens(>991px)
+  $(".filters .accordion-handler-filter").click(function() {
+    var clicked = $(this);
+    var target = $(this).attr("data-target");
+
+    if ($(".accordion-content-filters").attr("data-opened") === target) {
+      $(".accordion-content-filters").hide();
+      $(".accordion-content-filters").attr("data-opened", "");
+    } else {
+      $(".accordion-content-filters").show();
+      $(".accordion-content-filters").attr("data-opened", target);
+    }
+
+    $(".filters .accordion-handler-filter").each(function() {
+      if (!$(this).is(clicked)) {
+        $(this).removeClass("show-mode");
+      }
+    });
+
+    $(this).toggleClass("show-mode");
+
+    $(".accordion-content-filters .accordion-filter").each(function functionName() {
+      if ($(this).attr("data-filter") === target) {
+        $(this).toggle();
+      } else {
+        $(this).hide();
+      }
+    });
+
+    $(".sort .accordion-handler-filter").removeClass("show-mode");
+    $(".sort .accordion-handler-filter").next().hide();
+
+
+
+  });
+
+  // sort filter dropdown
+  $(".sort .accordion-handler-filter").click(function() {
+    var target = $(this).attr("data-target");
+
+    if ($(window).width() < 992) {
+
+      $(".accordion-content-filters .accordion-handler-filter").hide();
+      $(".accordion-content-filters .accordion-filter").hide();
+      $(".accordion-content-filters .accordion-handler-filter").removeClass("show-mode");
+      $(".filters .show-filters").removeClass("show-mode");
+
+      if ($(".accordion-content-filters").attr("data-opened") === target || $(".accordion-content-filters").attr("data-opened") === "") {
+        $(".accordion-content-filters").toggle();
+      }
+
+      $(".accordion-content-filters .accordion-filter-sort").toggle();
+
+      if ($(".accordion-content-filters").is(":hidden")) {
+        $(".accordion-content-filters").attr("data-opened", "");
+      } else {
+        $(".accordion-content-filters").attr("data-opened", target);
+      }
+
+      renderButton();
+
+      // $(".products-filter-top-level > .container > .row > .col-xs-6:first").toggleClass("add-border");
+
+
+    } else {
+
+      if ($(".accordion-content-filters").is(":hidden")) {
+        $(".accordion-content-filters").attr("data-opened", "");
+      } else {
+        $(".accordion-content-filters").attr("data-opened", target);
+      }
+
+
+
+      // close .filters
+      $(".accordion-content-filters").hide();
+      $(".accordion-content-filters .accordion-filter").hide();
+      $(".accordion-content-filters").attr("data-opened", "");
+      $(".filters .accordion-handler-filter").removeClass("show-mode");
+
+    }
+
+    $(this).toggleClass("show-mode");
+    $(this).next().toggle();
+
+
+  });
+
+  if ($(window).width() < 992) {
+    $(".sort-by").text("Sort by");
+  }
+
+  // sort dropdown list
+  $(".sort, .accordion-filter-sort").on("click", "a", function(e) {
+    e.preventDefault();
+    var sortBy = $(this).text();
+
+    $(".sort-by").text(sortBy);
+    $(this).parent().parent().hide();
+    $(".sort-by").parent().removeClass("show-mode");
+    $(".accordion-content-filters").hide();
+
+    // make the text fit on 320px
+    if ($(window).width() < 400) {
+      $(".sort-by").css("font-size", "11px");
+      $(".sort .accordion-handler-filter").css("padding-left", "0");
+    }
+
+
+  });
+
+  // close button for filters
+  $(".accordion-content-filters .close-btn").click(function() {
+    var opened = $(".accordion-content-filters").attr("data-opened", "");
+
+    $(".accordion-content-filters .accordion-filter").hide();
+    $(".accordion-content-filters").hide();
+    $(".accordion-content-filters").attr("data-opened", "");
+    $(".accordion-handler-filter").removeClass("show-mode");
+
+    $(".filters .show-filters").removeClass("show-mode");
+    $(".accordion-content-filters .accordion-handler-filter").hide();
+  });
+
+  // apply button for filters
+  $(".accordion-content-filters .apply-btn").click(function() {
+    var counter = 0;
+    $(".accordion-content-filters input[type=checkbox]").each(function() {
+
+      if ($(this).is(":checked")) {
+        counter++;
+        var text = $(this).siblings(".label").text();
+        var category = $(this).closest(".accordion-filter").attr("data-filter");
+
+        var singleFilter = $("<article></article>");
+        var removeFilter = $("<span></span>");
+        var outerSpan = $("<span></span>");
+        var filterCategory = $("<span></span>");
+        var textHere = $("<span></span>");
+
+        singleFilter.addClass("single-filter");
+        removeFilter.addClass("icon-font icon-close remove-filter");
+        filterCategory.addClass("filter-category text-capitalize");
+        textHere.addClass("text-capitalize");
+
+        textHere.text(text);
+        filterCategory.text(category + ": ");
+
+        outerSpan.prepend(filterCategory);
+        outerSpan.append(textHere);
+
+        singleFilter.prepend(removeFilter);
+        singleFilter.append(outerSpan);
+
+        $(".current-filters").prepend(singleFilter);
+        checkIfFiltersExist();
+
+      }
+      // $(this).prop("checked", false);
+      // $(this).next().removeClass("checked");
+
+    });
+
+    $(".accordion-content-filters").find(".accordion-filter").hide();
+    $(".accordion-content-filters").hide();
+    $(".accordion-content-filters").attr("data-opened", "");
+    $(".accordion-handler-filter").removeClass("show-mode");
+
+    $(".filters .show-filters").removeClass("show-mode");
+    $(".accordion-content-filters .accordion-handler-filter").hide();
+
+    renderButton();
+
+  });
+
+  // remove filter
+  $(".filters-applied").on("click", ".remove-filter", function() {
+    $(this).parent().remove();
+    var filter = $(this).next().find("span:last").text();
+
+    $(".accordion-content-filters input[type=checkbox]").each(function() {
+      if ($(this).siblings(".label").text() === filter) {
+        $(this).prop("checked", false);
+        $(this).next().removeClass("checked");
+      }
+    });
+
+    renderButton();
+    checkIfFiltersExist();
+  });
+
+  // remove all filters
+  $(".filters-applied .clear").click(function() {
+    $(".filters-applied .current-filters").html("");
+
+    $(".accordion-content-filters input[type=checkbox]").each(function() {
+      $(this).prop("checked", false);
+      $(this).next().removeClass("checked");
+    });
+
+    checkIfFiltersExist();
+  });
+
+  function checkIfFiltersExist() {
+    if ($(".filters-applied .current-filters .single-filter").length === 0) {
+      $(".filters-applied").hide();
+    } else {
+      $(".filters-applied").show();
+    }
+  }
+
+  // close cookies bar
+  $(".cookies-bar .button-cookies").click(function(e) {
+    e.preventDefault();
+    $(this).parent().hide();
   });
 
 });
